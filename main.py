210bf4ab2853216059b48be3b650937fd2b41536
M. Hamghalam <39214000+hamghalam@users.noreply.github.com>
2021-09-06 12:54:24 -0400

    recursion algorithm

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("hello world!")
-print("bye")
-print("salam")
-print("bye")
+def find_sum(n):
+    if n == 1:
+        return 1
+    return n + find_sum(n-1)
+
+if __name__ == '__main__':
+    print(find_sum(6))
