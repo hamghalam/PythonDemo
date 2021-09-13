0ba8c5b809cb18d4f7c9b623fe51299b39197937
M. Hamghalam <39214000+hamghalam@users.noreply.github.com>
2021-09-12 21:59:10 -0400

    code formatting

--- data_structure/data_handling.py
+++ data_structure/data_handling.py
@@ -1,10 +1,10 @@
 data_weather = {}
 data_weather_list = []
-with open ('data_structure/nyc_weather.csv','r') as f:
+with open("data_structure/nyc_weather.csv", "r") as f:
     for line in f:
-        tokens = line.split(',')
-        temp = (tokens[1])
-        if temp == 'temperature(F)\n':
+        tokens = line.split(",")
+        temp = tokens[1]
+        if temp == "temperature(F)\n":
             pass
         else:
             data_weather[tokens[0]] = float(temp)
@@ -13,24 +13,24 @@ with open ('data_structure/nyc_weather.csv','r') as f:
     print(data_weather)
     sum = 0
     n = 1
-    max_temp = max (data_weather_list)
-    for k,v in data_weather.items():
-       print(v)
-       if v==max_temp:
-           max_temp_jan = k
-       sum = v + sum
-       n += 1
-    print(f'avg = {sum/(n-1)}')
-    #print(sum,n-1)
+    max_temp = max(data_weather_list)
+    for k, v in data_weather.items():
+        print(v)
+        if v == max_temp:
+            max_temp_jan = k
+        sum = v + sum
+        n += 1
+    print(f"avg = {sum/(n-1)}")
+    # print(sum,n-1)
     print(max_temp_jan, max_temp)
 
-    print(data_weather['Jan 1'])
+    print(data_weather["Jan 1"])
 token_all = []
-with open ('data_structure/poem.txt','r') as f:
+with open("data_structure/poem.txt", "r") as f:
     for line in f:
         tokens = line.split()
-        token_all+=tokens
-    #print(token_all)
+        token_all += tokens
+    # print(token_all)
     dict_cnt = {}
     for element in token_all:
         if element not in dict_cnt:
@@ -38,4 +38,3 @@ with open ('data_structure/poem.txt','r') as f:
         else:
             dict_cnt[element] = dict_cnt[element] + 1
     print(dict_cnt)
-
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def find_sum(n):
     if n == 1:
         return 1
-    return n + find_sum(n-1)
+    return n + find_sum(n - 1)
 
 
 def fib(n):
@@ -12,49 +12,50 @@ def fib(n):
     if n == 0:
         return 0
     if n == 1:
-        return 1     
-    
-    return fib(n-1) + fib(n-2)
-# 
+        return 1
+    return fib(n - 1) + fib(n - 2)
 
-def summation (a:list):
+
+def summation(a: list):
     if len(a) == 1:
         return a[0]
 
     return a[-1] + summation(a[:-1])
 
 
-
-def convert_string(n:int, base):
-    table = "0123456789ABCDEF"
+def convert_string(n: int, base):
     """
     10 //2 = 5  0
-    5 // 2 = 2  1 
+    5 // 2 = 2  1
     2 // 2 = 1  0
     """
+    table = "0123456789ABCDEF"
     if n < base:
         return table[n]
-    return convert_string(n//base, base) + table[n % base]
+    return convert_string(n // base, base) + table[n % base]
 
-def sumint(a:int):
-    if( a < 10):
+
+def sumint(a: int):
+    if a < 10:
         return a
     b = a % 10
     a = a // 10
     return b + sumint(a)
 
-def funct1(n:int):
+
+def funct1(n: int):
     if n <= 0:
-        return 0   
+        return 0
+    return funct1(n - 2) + n
 
-    return funct1(n-2) + n
 
-def harmonicsum(n:int):
-    if n==1:
+def harmonicsum(n: int):
+    if n == 1:
         return 1
-    return harmonicsum(n-1) + (1/n)
+    return harmonicsum(n - 1) + (1 / n)
 
-class Graph():
+
+class Graph:
     def __init__(self, edges):
         self.graph_dict = {}
         for start, end in edges:
@@ -63,22 +64,23 @@ class Graph():
             else:
                 self.graph_dict[start] = [end]
 
-if __name__ == '__main__':
-    #print(fib(7))
-    #print(summation([1,2,10,11,55]))
-    #a = [1,2]
-    #print(a[:-1])
-    #print(convert_string(125,16))
-    #print(harmonicsum(3))
 
-    routes = [("Mumbai","Paris"),
-    ("Mumbai", "Dubai"),
-    ("Paris","Dubai"),
-    ("Paris","New York"),
-    ("Dubai","New York"),
-    ("New York","Toronto")   
+if __name__ == "__main__":
+    # print(fib(7))
+    # print(summation([1,2,10,11,55]))
+    # a = [1,2]
+    # print(a[:-1])
+    # print(convert_string(125,16))
+    # print(harmonicsum(3))
+
+    routes = [
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto"),
     ]
 
     route_graph = Graph(routes)
     print(route_graph.graph_dict)
-
